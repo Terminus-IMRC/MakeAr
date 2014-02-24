367b8f5114030ac7b115ba24662df3d7ed54c3f6
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-02-25 01:01:27 +0900

    MakeAr.py: implement whole base program

--- MakeAr.py
+++ MakeAr.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 import sys
 
-def func(level):
-	global X
+cursum=0
 
-	print(X, level)
+def func(level):
+	global X, ar, cursum
+
+	if level==X:
+		if cursum==X*(X**2+1)/2:
+			print(ar)
+		return
+
+	if level==0:
+		start=1
+	else:
+		start=ar[level-1]+1
+
+	for i in range(start, X**2+1):
+		ar[level]=i
+		cursum+=i
+		if cursum<=X*(X**2+1)/2:
+			func(level+1)
+		cursum-=i
 
 def main():
-	global X
+	global X, ar
 
 	if len(sys.argv)!=2:
 		error("specify X")
@@ -16,6 +33,8 @@ def main():
 		X=int(sys.argv[1])
 	except ValueError:
 		error("the value is unrecognized")
+
+	ar=[[]for x in range(X)]
 
 	func(0)
 
